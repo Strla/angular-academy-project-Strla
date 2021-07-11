c37f187552cff3ffb889efd3f321ce3f6492d98d
Ivan Starčević <ivan.starcevic15@gmail.com>
2021-07-12 00:12:21 +0200

    Add all components for homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AllShowsContainerComponent } from './pages/all-shows-container/all-shows-container.component';
+import { ShowListComponent } from './components/show-list/show-list.component';
+import { ShowCardComponent } from './components/show-card/show-card.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
+	declarations: [AppComponent, MainLayoutComponent, SidenavComponent, AllShowsContainerComponent, ShowListComponent, ShowCardComponent],
+	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatSidenavModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
