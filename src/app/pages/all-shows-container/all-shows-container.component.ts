ac19d44a5b7c27d549509ea336021d9ad6b7a17d
Ivan Starčević <ivan.starcevic15@gmail.com>
2021-07-12 02:05:57 +0200

    Add a few more TV shows

--- src/app/pages/all-shows-container/all-shows-container.component.ts
+++ src/app/pages/all-shows-container/all-shows-container.component.ts
@@ -25,10 +25,31 @@ export class AllShowsContainerComponent {
 		},
 		{
 			title: 'The Wire',
-			description: 'A show about Baltimore criminal ring in early 2000s',
+			description: 'A TV show about Baltimore criminal ring in early 2000s',
 			average_rating: 9.3,
 			image_url:
 				'https://hbomax-images.warnermediacdn.com/images/GVU2gkAgT4oNJjhsJAUDk/tileburnedin?size=1280x720&partner=hbomaxcom&host=artist.api.cdn.hbo.com&w=1280',
+		},
+		{
+			title: 'Vikings',
+			description: 'A drama/action TV show about Ragnar and his sons',
+			average_rating: 8.7,
+			image_url: 'https://i.pinimg.com/564x/cb/ff/b7/cbffb7975e92c065ecc6b4516269b62c.jpg',
+		},
+		{
+			title: 'Game of Thrones',
+			description: 'A fantasy TV show based on the novel of the same name by George R R Martin',
+			average_rating: 9.4,
+			image_url:
+				'https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg',
+		},
+		{
+			title: 'The Man in the High Castle',
+			description:
+				'TV show about dystopian version of reality where Nazis won the war based on the novel of the same name',
+			average_rating: 8.2,
+			image_url:
+				'https://m.media-amazon.com/images/M/MV5BZWEwNzQ4NzUtMWRmYS00NDdiLTg5NDItODA5M2M4YTM0ZTE2XkEyXkFqcGdeQXVyMTAzNjU2NjM1._V1_.jpg',
 		},
 		{
 			title: 'Rick and Morty',
