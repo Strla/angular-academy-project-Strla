df6b721688cbd3b3c24cde2027e4838de4338f7a
Ivan Starčević <ivan.starcevic15@gmail.com>
2021-07-26 15:41:40 +0200

    Add auth interceptor

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication/authentication.service';
+import { AuthData } from '../interfaces/auth-data.interface';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+	constructor(private authService: AuthenticationService) {}
+
+	intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+		const authData: AuthData | null = this.authService.getAuthData();
+
+		let finalRequest: HttpRequest<unknown> = request;
+
+		if (authData) {
+			finalRequest = request.clone({
+				headers: new HttpHeaders({
+					'access-token': authData.token,
+					client: authData.client,
+					uid: authData.uid,
+				}),
+			});
+		}
+
+		console.log(finalRequest);
+		return next.handle(request);
+	}
+}
