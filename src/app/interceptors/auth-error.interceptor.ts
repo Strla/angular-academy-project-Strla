1ea771ae2ebebd29e099c3d6ac14de5171ceacfd
Ivan Starčević <ivan.starcevic15@gmail.com>
2021-07-27 14:57:05 +0200

    Add EMPTY to auth error interceptor

--- src/app/interceptors/auth-error.interceptor.ts
+++ src/app/interceptors/auth-error.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { EMPTY, Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/internal/operators';
 import { Router } from '@angular/router';
 
@@ -12,6 +12,7 @@ export class AuthErrorInterceptor implements HttpInterceptor {
 			catchError((error: HttpErrorResponse) => {
 				if (error.status === 401 || error.status === 403) {
 					this.router.navigate(['/login']);
+					return EMPTY;
 				}
 				return throwError(error);
 			})
